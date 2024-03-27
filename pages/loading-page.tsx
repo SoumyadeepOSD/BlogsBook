8dd2fb3d1603bc0fb6ae5eb3d3322544cc40ed05
SoumyadeepOSD <opencontainer255@gmail.com>
2024-03-28 01:36:51 +0530

    Default export changed

--- pages/loading-page.tsx
+++ pages/loading-page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { Spinner } from "@nextui-org/react";
 
-export const LoadingPage = () => {
+const LoadingPage = () => {
     return(
         <div className="h-screen w-screen bg-white flex flex-col items-center justify-center">
             <Spinner size="lg" color="secondary"/>
         </div>
     );  
 }
+
+export default LoadingPage;
